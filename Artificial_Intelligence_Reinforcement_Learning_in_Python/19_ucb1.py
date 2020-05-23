d858b1d25df2c013e107bff49810e0f3ad00372f
Yu-Ting <12582241+ytatus94@users.noreply.github.com>
2020-05-23 16:04:04 -0700

    Create 19_ucb1.py

---
+++ Artificial_Intelligence_Reinforcement_Learning_in_Python/19_ucb1.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+NUM_TRIALS = 100000
+EPS = 0.1
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+
+class Bandit:
+    def __init__(self, p):
+        # p: the win rate
+        self.p = p
+        self.p_estimate = 0.
+        self.N = 0 # num samples collected so far
+
+    def pull(self):
+        # draw a 1 with probability p
+        return np.random.random() < self.p
+
+    def update(self, x):
+        self.N += 1.
+        self.p_estimate = ((self.N - 1) * self.p_estimate + x) / self.N
+
+def ucb(mean, n, nj): # n: 總共玩吃角子老虎機的次數 nj: 玩第 j 台吃角子老虎機的次數
+    return mean + np.sqrt(2 * np.log(n) / nj)
+
+def run_experiment():
+    bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+    rewards = np.empty(NUM_TRIALS)
+    total_plays = 0
+
+    # initialization: play each bandit once
+    for j in range(len(bandits)): # 每一台吃角子老虎機都先玩一次，可以預防 ucb 中有 n=0/nj=0 的情形
+        x = bandits[j].pull()
+        total_plays += 1
+        bandits[j].update(x)
+
+    for i in range(NUM_TRIALS): # 這才是主程式
+        j = np.argmax([ucb(b.p_estimate, total_plays, b.N) for b in bandits])
+        x = bandits[j].pull()
+        total_plays += 1
+        bandits[j].update(x)
+
+        # for the plot
+        rewards[i] = x
+    cumulative_average = np.cumsum(rewards) / (np.arange(NUM_TRIALS) + 1)
+
+    # plot moving average ctr
+    plt.plot(cumulative_average)
+    plt.plot(np.ones(NUM_TRIALS) * np.max(BANDIT_PROBABILITIES))
+    plt.xscale('log')
+    plt.show()
+
+    # plot moving average ctr linear
+    plt.plot(cumulative_average)
+    plt.plot(np.ones(NUM_TRIALS) * np.max(BANDIT_PROBABILITIES))
+    plt.show()
+
+    for b in bandits:
+        print(b.p_estimate)
+
+    print('total reward earned:', rewards.sum())
+    print('overall win rate:', rewards.sum() / NUM_TRIALS)
+    print('num times selected each bandit:', [b.N for b in bandits])
+
+    return cumulative_average
+
+if __name__ == '__main__':
+    run_experiment()
